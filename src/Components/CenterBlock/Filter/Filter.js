154ea0f6b0496c5598a682870bc7b1265e0fb0d6
Mark-Levi <nessy-777@yandex.ru>
2023-07-03 21:22:38 +0300

    Не работают иконки

--- src/Components/CenterBlock/Filter/Filter.js
+++ src/Components/CenterBlock/Filter/Filter.js
@@ -1,12 +1,8 @@
 import {useState} from "react";
 import styles from './Filter.module.css';
 import FilterItem from "./FilterItem/FilterItem";
-// import './Filter.module.css';
-
-
 
 // Подготовка тестового плейлиста
-
 import tempPlayList from "../../../tempPlayList";
 
 const songFilter=tempPlayList.map(song=><a className={styles.filterItems} href="http://" key={song.id}>{song.name}</a>);
@@ -16,19 +12,16 @@ let genreFilter=Array.from(new Set(tempPlayList.map(song=>song.genre))).sort();
     genreFilter=genreFilter.map((genre)=><a className={styles.filterItems} href="http://" key={genre}>{genre}</a>);
 
 function Filter()
-{
-    const [buttons,setButtons]=useState([false,false,false])
-
-   
-
-    return(
-        
-        <div className={styles.main}>
-            <div className={styles.filter__title}>Искать по:</div>
-            <FilterItem buttons={buttons} setButtons={setButtons} id={0} menuList= {songFilter} buttonName="исполнителю"/>
-            <FilterItem buttons={buttons} setButtons={setButtons} id={1} menuList= {yearFilter} buttonName="году выпуска"/>
-            <FilterItem buttons={buttons} setButtons={setButtons} id={2} menuList= {genreFilter} buttonName="жанру"/>
-        </div>
-    )
-}
+    {
+        const [buttons,setButtons]=useState([false,false,false])
+        return(
+            
+            <div className={styles.main}>
+                <div className={styles.filter__title}>Искать по:</div>
+                <FilterItem buttons={buttons} setButtons={setButtons} id={0} menuList= {songFilter} buttonName="исполнителю"/>
+                <FilterItem buttons={buttons} setButtons={setButtons} id={1} menuList= {yearFilter} buttonName="году выпуска"/>
+                <FilterItem buttons={buttons} setButtons={setButtons} id={2} menuList= {genreFilter} buttonName="жанру"/>
+            </div>
+        )
+    }
 export default Filter
--- src/Components/Footer/Player/PlayerControls/PlayerControls.js
+++ src/Components/Footer/Player/PlayerControls/PlayerControls.js
@@ -1,5 +1,5 @@
 // import React from "react";
-import './style.css';
+import './PlayerControls.module.css';
 import Icon from '../../../Icon/Icon'
 
 
