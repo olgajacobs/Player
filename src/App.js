93bfe85230aaa756be1755ba4634253ec23d3a71
Mark-Levi <nessy-777@yandex.ru>
2023-07-04 10:30:06 +0300

    Prettier work

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import {useState,useEffect} from 'react'
-import styles from './App.module.css';
-import LeftBlockMenu from './Components/LeftBlockMenu/LeftBlockMenu';
-import CenterBlock from './Components/CenterBlock/CenterBlock';
+import { useState, useEffect } from "react";
+import styles from "./App.module.css";
+import LeftBlockMenu from "./Components/LeftBlockMenu/LeftBlockMenu";
+import CenterBlock from "./Components/CenterBlock/CenterBlock";
 import Footer from "./Components/Footer/Footer";
-import RightBlock from './Components/RightBlock/RightBlock';
+import RightBlock from "./Components/RightBlock/RightBlock";
 
-    
 function App() {
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      setTimeout(() => setLoading(false), 4000);
-    });
-    return (
-        <div className={styles.container} >
-            <div className={styles.main}>
-                <LeftBlockMenu loading={loading}/>
-                <CenterBlock loading={loading}/>
-                <RightBlock loading={loading}/>
-            </div>
-            <Footer loading={loading}/>
+  const [loading, setLoading] = useState(true);
 
-            {loading&&(  
-            <div className={styles.shadow} >
-                <p >Loading...</p>
-            </div>)}
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 4000);
+  });
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <LeftBlockMenu loading={loading} />
+        <CenterBlock loading={loading} />
+        <RightBlock loading={loading} />
+      </div>
+      <Footer loading={loading} />
+
+      {loading && (
+        <div className={styles.shadow}>
+          <p>Loading...</p>
         </div>
-      );
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/Components/RightBlock/RightBlock.js
+++ src/Components/RightBlock/RightBlock.js
@@ -2,24 +2,21 @@
 import RightBlockItem from './RightBlockItem/RigthBlockItem';
 import styles from './RightBlock.module.css';
 
+function RightBlock({ loading }) {
+  const personal = loading ? '' : 'Sergey.Ivanov';
 
-function RightBlock({loading}){
-    const personal =loading? "":"Sergey.Ivanov"
-  
-    return(
-      <div className={styles.main}>
-        <div className={styles.user}>
-              <p className={styles.user__name}>{personal}</p>
-              <div className={styles.user__avatar} />
-            </div>
-        <div className={styles.playlist__block}>
-            <RightBlockItem loading={loading} fileName="playlist01" altName="day's playlist"/>
-            <RightBlockItem loading={loading} fileName="playlist02" altName="day's playlist"/>
-            <RightBlockItem loading={loading} fileName="playlist03" altName="day's playlist"/>
-        </div>
+  return (
+    <div className={styles.main}>
+      <div className={styles.user}>
+        <p className={styles.user__name}>{personal}</p>
+        <div className={styles.user__avatar} />
       </div>
-        
-    )
-
-    }
-    export default RightBlock
+      <div className={styles.playlist__block}>
+        <RightBlockItem loading={loading} fileName='playlist01' altName="day's playlist" />
+        <RightBlockItem loading={loading} fileName='playlist02' altName="day's playlist" />
+        <RightBlockItem loading={loading} fileName='playlist03' altName="day's playlist" />
+      </div>
+    </div>
+  );
+}
+export default RightBlock;
