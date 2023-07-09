859ff0541c7637007502e5d2444a23b537efb548
Mark-Levi <nessy-777@yandex.ru>
2023-07-09 16:41:39 +0300

    Начало стилизованных компонетов

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { useState, useEffect } from "react";
-import styles from "./App.module.css";
-import LeftBlockMenu from "./Components/LeftBlockMenu/LeftBlockMenu";
-import CenterBlock from "./Components/CenterBlock/CenterBlock";
-import Footer from "./Components/Footer/Footer";
-import RightBlock from "./Components/RightBlock/RightBlock";
+import { useState, useEffect } from 'react'
+import styles from './App.module.css'
+import LeftBlockMenu from './Components/LeftBlockMenu/LeftBlockMenu'
+import CenterBlock from './Components/CenterBlock/CenterBlock'
+import Footer from './Components/Footer/Footer'
+import RightBlock from './Components/RightBlock/RightBlock'
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 4000);
-  });
+    setTimeout(() => setLoading(false), 4000)
+  })
   return (
     <div className={styles.container}>
       <div className={styles.main}>
         <LeftBlockMenu loading={loading} />
+
         <CenterBlock loading={loading} />
         <RightBlock loading={loading} />
       </div>
@@ -26,7 +27,7 @@ function App() {
         </div>
       )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
