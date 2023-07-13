6e5df5cf699d353c7eff7e7b3ecc635bdb50c7ac
Mark-Levi <nessy-777@yandex.ru>
2023-07-13 18:54:59 +0300

    Непонятная ошибка с css2

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Main from './pages/main'
 import Registration from './pages/registration'
-import Login from './pages/login'
+import Login from './pages/autorization'
 import NotFound from './pages/notfound'
 // import styles from './Login.module.css'
 
@@ -9,7 +9,7 @@ function AppRouts() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/autorization" element={<Login />} />
       <Route path="/registration" element={<Registration />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
