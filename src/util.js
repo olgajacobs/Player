016786eefaf59ff370ec8f66f50b86e1e73814ed
Mark-Levi <nessy-777@yandex.ru>
2023-08-08 10:23:39 +0300

    Настройка перемешивания массива 2

--- src/util.js
+++ src/util.js
@@ -17,7 +17,7 @@ export const shuffle = (array) => {
   // Тасование Фишера — Йетса
   for (let i = array.length - 1; i > 0; i -= 1) {
     const j = Math.floor(Math.random() * (i + 1))
-    ;[newArray[i], newArray[j]] = [array[j], array[i]]
+    ;[newArray[i], newArray[j]] = [newArray[j], newArray[i]]
   }
   return newArray
 }
