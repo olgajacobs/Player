87ea74a1343b0f2ab3f6bebef7cc045d396d48cf
Mark-Levi <nessy-777@yandex.ru>
2023-10-07 12:44:24 +0300

    new api

--- src/RTKapi.js
+++ src/RTKapi.js
@@ -1,23 +1,89 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { setAuth } from './store/slices/auth'
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  const baseQuery = fetchBaseQuery({
+    baseUrl: 'https://skypro-music-api.skyeng.tech',
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth.access
+      // Чтобы выключить отображение debug логов в консоли браузера, включите уровень Verbose\Debug в консоли разработчика
+      console.debug('Использую токен из стора', { token })
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`)
+      }
+      return headers
+    },
+  })
+
+  // Делаем запрос
+  const result = await baseQuery(args, api, extraOptions)
+  console.debug('Результат первого запроса', { result })
+
+  // Если запрос выполнился не с 401 кодом, то все хорошо, просто отдаем результат запроса наружу
+  if (result?.error?.status !== 401) {
+    return result
+  }
+
+  // Ниже обрабатываем 401 код
+  // Функция которая отчищает данные о юзере в сторе и отправляет на страницу логина
+  const forceLogout = () => {
+    console.debug('Принудительная авторизация!')
+    api.dispatch(setAuth(null))
+    window.location.navigate('/login')
+  }
+
+  // Функция getState возвращает состояние redux стейта целиком, ее нам предоставляет rtk query, она прилетает параметром запроса в функцию
+  const { auth } = api.getState()
+  console.debug('Данные пользователя в сторе', { auth })
+  // Если в сторе нет refresh токена, то помочь пользователю мы уже ничем не сможем, разлогиниваем его и отправляем авторизоваться руками
+  if (!auth.refresh) {
+    return forceLogout()
+  }
+
+  // Делаем запрос за новым access токеном в API обновления токена
+  const refreshResult = await baseQuery(
+    {
+      url: '/user/token/refresh/',
+      method: 'POST',
+      body: {
+        refresh: auth.refresh,
+      },
+    },
+    api,
+    extraOptions
+  )
+
+  console.debug('Результат запроса на обновление токена', { refreshResult })
+  if (!refreshResult.data.access) {
+    return forceLogout()
+  }
+
+  // Мы наконец получили новый access токен, сохраняем его в стор, чтобы последующие запросы могли его использовать внутри prepareHeaders
+  api.dispatch(setAuth({ ...auth, access: refreshResult.data.access }))
+
+  const retryResult = await baseQuery(args, api, extraOptions)
+
+  // Если повторный запрос выполнился с 401 кодом, то что-то совсем пошло не так, отправляем на принудительную ручную авторизацию
+  if (retryResult?.error?.status === 401) {
+    return forceLogout()
+  }
+
+  console.debug('Повторный запрос завершился успешно')
+
+  return retryResult
+}
 
 const TRACKS_TAG = 'Tracks'
-const getTokenFromLocalStorage = () =>
-  JSON.parse(localStorage.getItem('accessToken'))
 
 export const RTKApi = createApi({
   reducerPath: 'RTKApi',
   tagTypes: ['Favorits'],
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://skypro-music-api.skyeng.tech/catalog/',
-  }),
+  baseQuery: baseQueryWithReauth,
   endpoints: (build) => ({
     getFavorites: build.query({
       query: () => ({
         url: `track/favorite/all/`,
         method: 'GET',
-        headers: {
-          Authorization: `Bearer ${getTokenFromLocalStorage()}`,
-        },
       }),
       providesTags: () => [TRACKS_TAG],
     }),
@@ -25,9 +91,6 @@ export const RTKApi = createApi({
       query: ({ id }) => ({
         url: `track/${id}/favorite/`,
         method: 'DELETE',
-        headers: {
-          Authorization: `Bearer ${getTokenFromLocalStorage()}`,
-        },
       }),
       invalidatesTags: [TRACKS_TAG],
     }),
@@ -36,9 +99,6 @@ export const RTKApi = createApi({
         url: `track/${id}/favorite/`,
         method: 'POST',
         body,
-        headers: {
-          Authorization: `Bearer ${getTokenFromLocalStorage()}`,
-        },
       }),
       invalidatesTags: [TRACKS_TAG],
     }),
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import pleerReducer from './slices/pleer'
+import authReducer from './slices/auth'
 import { RTKApi } from '../RTKapi'
 
 const store = configureStore({
   reducer: {
     pleer: pleerReducer,
+    auth: authReducer,
     [RTKApi.reducerPath]: RTKApi.reducer,
   },
   middleware: (getDefaultMiddlware) =>
